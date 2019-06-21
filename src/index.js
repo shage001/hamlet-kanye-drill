0b1291beb6541ea317663728f90a12ba4593254e
Sam Hage <samhage@sams-mbp.usny.ibm.com>
2019-06-21 15:30:28 -0400

    Get things working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import hamlet from './assets/img/hamlet.jpg';
+import kanye from './assets/img/kanye.jpg';
+import dril from './assets/img/dril.jpg';
 
 // https://community.ibm.com/community/user/imwuc/blogs/tiago-moura/2018/07/17/5-min-deployment-react-web-app-running-on-ibmcloud
 
 const twitterServiceURL = 'https://us-south.functions.cloud.ibm.com/api/v1/web/samhage%40ibm.com_dev/default/twitter_guesser.json';
+const profileMap = {
+    'hamlet': {'image': hamlet, 'username': 'Prince Hamlet', 'handle': '@hamletthedane'},
+    'kanye': {'image': kanye, 'username': 'ye', 'handle': '@kanyewest'},
+    'dril': {'image': dril, 'username': 'wint', 'handle': '@dril'},
+};
 
 
 class Button extends React.Component {
     render() {
         return (
             <button 
+                id={this.props.name}
                 className="name-button"
-                onClick={() => this.props.onClick()}
+                onMouseOver={(e) => this.props.onMouseOver(e)}
+                onClick={(e) => this.props.onClick(e)}
             >
                 {this.props.name}
             </button>
@@ -26,12 +36,46 @@ class Game extends React.Component {
         super(props);
         this.state = {
             curTweet: {text: 'loading...', author: 'sam'},
-            curProfile: 'hamlet',
+            curProfile: ['hamlet', 'kanye', 'dril'][Math.floor(Math.random() * 3)],
+            numGuesses: 0,
+            numCorrect: 0,
+            scoreColor: '#000000',
         };
     }
 
     componentWillMount() {
         this.getTweetAndUpdateState();
+    }
+
+    handleMouseOver(e) {
+        this.setState({
+            curProfile: e.target.id
+        });
+    }
+
+    handleClick(e) {
+        const guess = e.target.id;
+        const actual = this.state.curTweet.author;
+        var correct = guess === actual ? true : false;
+
+        var newCorrect = this.state.numCorrect;
+        if (correct) {
+            newCorrect++;
+        }
+        const color = determineColor(newCorrect, this.state.numGuesses + 1);
+        this.setState({ scoreColor: color });
+
+        this.setState({ numGuesses: this.state.numGuesses + 1 });
+        this.setState({ numCorrect: newCorrect });
+
+        this.getTweetAndUpdateState();
+    }
+
+    getTweetAndUpdateState() {
+        this.getTweet().then(res => {
+            console.log(res);
+            this.setState({ curTweet: res });
+        });
     }
 
     getTweet = async () => {
@@ -47,35 +91,35 @@ class Game extends React.Component {
         return body;
     };
 
-    handleClick() {
-        this.getTweetAndUpdateState();
-    }
-
-    getTweetAndUpdateState() {
-        this.getTweet().then(res => {
-            console.log(res);
-            this.setState({ curTweet: res });
-        });
-    }
-
     render() {
         return (
             <div className="game text-center">
                 <div className="header">
-                    <h1>Hamelt, Kanye, or Dril?</h1>
+                    <h1>Hamlet, Kanye, or Dril?</h1>
+                </div>
+                <div className="buttons">
+                    <Button name="hamlet" onMouseOver={(e) => this.handleMouseOver(e)} onClick={(e) => this.handleClick(e)}/>
+                    <Button name="kanye" onMouseOver={(e) => this.handleMouseOver(e)} onClick={(e) => this.handleClick(e)}/>
+                    <Button name="dril" onMouseOver={(e) => this.handleMouseOver(e)} onClick={(e) => this.handleClick(e)}/>
                 </div>
                 <div className="tweet-box">
-                    <div className="profile-pic">
-                        <img src={require("hamlet.jpg")}/>
+                    <div className="top-bar">
+                        <div className="profile-pic">
+                            <img src={profileMap[this.state.curProfile].image} alt="profile-pic"/>
+                        </div>
+                        <div className="account-info">
+                            <span className="username">{profileMap[this.state.curProfile].username}</span>
+                            <span className="handle">{profileMap[this.state.curProfile].handle}</span>
+                        </div>
                     </div>
                     <div className="tweet">
                         {this.state.curTweet.text.toLowerCase()}
                     </div>
                 </div>
-                <div className="buttons">
-                    <Button name="dril" onClick={() => this.handleClick()}/>
-                    <Button name="kanye" onClick={() => this.handleClick()}/>
-                    <Button name="hamlet" onClick={() => this.handleClick()}/>
+                <div className="score">
+                    <span style={{color: this.state.scoreColor}}>
+                        {this.state.numCorrect} / {this.state.numGuesses}
+                    </span>
                 </div>
             </div>
         );
@@ -87,3 +131,16 @@ ReactDOM.render(
   <Game/>,
   document.getElementById('root')
 );
+
+
+function determineColor(numCorrect, numGuesses) {
+    const colors = ['#FF0000', '#FF1000', '#FF2000', '#FF3000', '#FF4000', '#FF5000', '#FF6000', '#FF7000', '#FF8000', '#FF9000', '#FFA000', '#FFB000', '#FFC000', '#FFD000', '#FFE000', '#FFF000', '#FFFF00', '#F0FF00', '#E0FF00', '#D0FF00', '#C0FF00', '#B0FF00', '#A0FF00', '#90FF00', '#80FF00', '#70FF00', '#60FF00', '#50FF00', '#40FF00', '#30FF00', '#20FF00', '#10FF00'];
+    if (numGuesses === 0) {
+        return '#000000';
+    }
+    else if (numCorrect === 0) {
+        return '#FF0000';
+    }
+    const rate = Math.round((numCorrect / numGuesses) * (colors.length - 1));
+    return colors[rate];
+}
